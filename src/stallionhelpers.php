c561e7c71d1004d9f8b925926fa817f376c79b34
Avinash Kant <avinashkant@stallionexpress.ca>
2023-06-09 11:36:45 +0530

    helper function

--- src/stallionhelpers.php
+++ src/stallionhelpers.php
@@ -1,7 +1,20 @@
 <?php
 
+use StallionExpress\AuthUtility\Enums\UserTypeEnum;
+use StallionExpress\AuthUtility\Models\User;
 
-function sayTest()
-    {
-        echo "I am doing testing";
+function getCustomerId(User $user, int $customerId = null)
+{
+    // check only for 3pl customers or 3pl customers staff
+    switch ($user->user_type->value) {
+        case UserTypeEnum::THREE_PL_CUSTOMER_STAFF->value:
+            $customerId = $user->three_pl_customer[0]->id;
+            break;
+        case UserTypeEnum::THREE_PL_CUSTOMER->value:
+            $customerId = $user->id;
+            break;
+
     }
+
+    return $customerId;
+}
