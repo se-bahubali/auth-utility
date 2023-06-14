2b1f1c8ebc2f31ea6d7e3621bf4eed50609c174e
Avinash Kant <avinashkant@stallionexpress.ca>
2023-06-14 17:16:19 +0530

    login

--- routes/stallion.php
+++ routes/stallion.php
@@ -6,7 +6,7 @@ use StallionExpress\AuthUtility\Controllers\StallionAuthController;
 Route::middleware(['decode_st'])->group(function () {
     Route::get('/auth/callback', [StallionAuthController::class, 'getAccessToken']);
 
-    Route::get('/login', [StallionAuthController::class, 'login']);
+    Route::get('/login', [StallionAuthController::class, 'login'])->name('authLogin');
 
     Route::get('api/stallion/access/token/{id}', [StallionAuthController::class, 'returnToken']);
 
