e9a68e7b187d9b21dad430ca4e0b597c2d8385df
Avinash kant <avinashkant@stallionexpress.ca>
2023-06-16 18:22:32 +0530

    Changed hasAbility method
    (cherry picked from commit 7372f89b2af9828bd4190e8e7691d6826f0079f0)

--- src/AuthUtility.php
+++ src/AuthUtility.php
@@ -11,34 +11,17 @@ class AuthUtility
     use STEncodeDecodeTrait;
     // Build your next great package.
 
-    public function hasAbility(User $user, string $feature, string $action, object $modelObject = null)
+    /**
+     * Check user has ability for feature
+     * @param User $user
+     * @param string $feature
+     * @param string $action
+     * @return bool
+     * 
+     * 1) User has ability for feature
+     */
+    public function hasAbility(User $user, string $feature, string $action):bool
     {    
-        $hasAbility = false;
-
-        // Check user has ability for feature
-        if (isset($user->abilities->{$feature}) === true && in_array($action, $user->abilities->{$feature}) === true) {
-            
-            //check for model dependent authorization
-            if($modelObject){
-                // Check user is in same 3pl 
-                if ($user->three_pl->hash == $this->decodeHashValue($modelObject->three_pl_id)) {
-                    $hasAbility = true;
-
-                    // Check user is in same 3pl customer
-                    if(in_array($user->user_type->value, [(UserTypeEnum::THREE_PL_CUSTOMER)->value, (UserTypeEnum::THREE_PL_CUSTOMER_STAFF)->value])){
-                        
-                        
-                        if ($user->three_pl_customer->hash == $this->decodeHashValue($modelObject->three_pl_customer_id)) {
-                            $hasAbility = true;
-                        }else{
-                            $hasAbility = false;
-                        }
-                    }
-                }
-            }else{
-                $hasAbility = true;
-            }           
-        }
-        return $hasAbility;
+        return isset($user->abilities->{$feature}) === true && in_array($action, $user->abilities->{$feature}) === true;
     }
 }
