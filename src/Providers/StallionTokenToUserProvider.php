eaa59cb4f4a982c715b77dc3c8b4e062c49d7fe1
Avinash kant <avinashkant@stallionexpress.ca>
2023-06-19 19:04:36 +0530

    Warehouses decode

--- src/Providers/StallionTokenToUserProvider.php
+++ src/Providers/StallionTokenToUserProvider.php
@@ -25,17 +25,23 @@ class StallionTokenToUserProvider implements UserProvider
                 'Accept' => 'application/json',
             ])->get(config('stallionauthutility.authservice.url') . '/api/oauth/token/user');
 
+        $user = null;
         if ($response->successful()) {
             $data = $response->object();
             $user = new User;
 
-            if(isset($data->warehouses) && is_array($data->warehouses)){
+            $warehouses = [];
+            if(isset($data->warehouses)){
                 foreach ($data->warehouses as $key => $warehouse) {
-                    $data->warehouses[$key]->hash = $this->decodeHashValue($warehouse->hash);
+                    $warehouses[$this->decodeHashValue($key)] = [
+                        'name' => $warehouse->name,
+                        'hash' => $this->decodeHashValue($warehouse->hash)
+                    ];
                 }
             }
+
             [$user->id, $user->hash, $user->warehouses, $user->details, $user->user_type, $user->abilities] = 
-            [$this->decodeHashValue($data->hash), $data->hash, $data->warehouses, $data->details, $data->user_type, $data->scopes];
+            [$this->decodeHashValue($data->hash), $data->hash, $warehouses, $data->details, $data->user_type, $data->scopes];
 
             if($data->user_type == UserTypeEnum::THREE_PL_STAFF->value){
                 $user->three_pl = $data->three_pl;
