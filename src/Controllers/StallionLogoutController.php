2fc3439b9a9c0a5bcb1105dcbe84aa0873f26ac5
se-bahubali <131521059+se-bahubali@users.noreply.github.com>
2023-07-28 12:10:23 +0530

    Logout api (#11)
    * Logout api
    
    * Logout api

---
+++ src/Controllers/StallionLogoutController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace StallionExpress\AuthUtility\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+use StallionExpress\AuthUtility\Trait\STEncodeDecodeTrait;
+
+class StallionLogoutController extends Controller
+{
+    use STEncodeDecodeTrait;
+
+    public $url;
+
+
+    public function __construct()
+    {
+        $this->url = config('stallionauthutility.authservice.url');
+    }
+
+    
+    public function logout(Request $request)
+    {
+        $bearer = $request->bearerToken();
+        if ($bearer) {
+            $response = Http::withToken($bearer)
+                ->withHeaders([
+                    'Accept' => 'application/json',
+                ])->get(config('stallionauthutility.authservice.url') . '/api/logout');
+
+            if ($response->successful()) {
+                return response()->json([
+                    'data' => true,
+                    'meta' => [
+                        'message' => 'You have been logged out successfully.'
+                    ],
+                ]);
+            }
+        }
+
+        return response()->json([
+            'data' => false,
+            'meta' => [
+                'message' => 'Unauthenticated.'
+            ],
+        ], 401);
+    }
+}
