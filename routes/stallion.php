51e4ea6276cbc0710743d60684b0267846be18ec
Avinash kant <avinashkant@stallionexpress.ca>
2023-07-28 11:43:45 +0530

    Logout api

--- routes/stallion.php
+++ routes/stallion.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use StallionExpress\AuthUtility\Controllers\StallionAuthController;
+use StallionExpress\AuthUtility\Controllers\StallionLogoutController;
 
 Route::middleware(['decode_st'])->group(function () {
     Route::get('/auth/callback', [StallionAuthController::class, 'getAccessToken']);
@@ -12,4 +13,5 @@ Route::middleware(['decode_st'])->group(function () {
 
     Route::get('api/stallion/{role}/scopes', [StallionAuthController::class, 'userScopes'])->name('returnScopes');
 
+    Route::get('api/logout', [StallionLogoutController::class, 'logout'])->name('logout');
 });
