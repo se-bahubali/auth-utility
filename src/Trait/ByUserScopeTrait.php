15b6be4ea98cbaeab11da1136c8ac89d64c9670c
se-bahubali <131521059+se-bahubali@users.noreply.github.com>
2023-12-21 20:42:27 +0530

    Update ByUserScopeTrait.php

--- src/Trait/ByUserScopeTrait.php
+++ src/Trait/ByUserScopeTrait.php
@@ -10,12 +10,13 @@ trait ByUserScopeTrait
     {
         return $query->when($userId && AuthUtility::isUser3plOr3plStaff($user), function ($query) use ($userId) {
             $query->where('user_id', $userId); // 3pl wants to check client record
+        }, function ($query) use ($user) {
+            $query->when(\AuthUtility::isUser3plOr3plStaff($user), function ($query) use ($user) {
+                $query->where('user_id', \AuthUtility::getThreePlIdFor3PlAndStaff($user));
+            });
         })
         ->when(\AuthUtility::isUserCustomerOrCustomerStaff($user), function ($query) use ($user) {
             $query->where('user_id', \AuthUtility::getCustomerIdFor3PlCustomerAndStaff($user));
-        })
-        ->when(\AuthUtility::isUser3plOr3plStaff($user), function ($query) use ($user) {
-            $query->orWhere('user_id', \AuthUtility::getThreePlIdFor3PlAndStaff($user));
-        });;
+        });
     }
 }
